f61d99d2ed55f81efcbcf5e016df88a491292a4d
M1sa-js <68118101+M1sa-js@users.noreply.github.com>
2026-06-18 05:30:58 +0800

    add llm_translate_nothink.js

---
+++ rewrite/llm_translate_nothink.js
@@ -0,0 +1,31 @@
+/*
+[rewrite_local]
+# 拦截 OpenRouter API 并注入禁用思考参数
+^https?:\/\/openrouter\.ai\/api\/v1\/chat\/completions url script-request-body https://raw.githubusercontent.com/M1sa-js/rule_script/refs/heads/main/rewrite/llm_translate_nothink.js
+[mitm]
+hostname = openrouter.ai
+*/
+
+
+let headers = $request.headers;
+let ua = headers['User-Agent'] || headers['user-agent'] || '';
+
+if (ua.includes('Easydict') && $request.body) {
+    try {
+        let body = JSON.parse($request.body);
+
+        body.reasoning = {
+            "enabled": false,
+            "exclude": true
+        };
+        body.include_reasoning = false;
+        body.reasoning_format = "none";
+
+        $done({ body: JSON.stringify(body) });
+    } catch (e) {
+        console.log("Easydict JSON 解析异常: " + e);
+        $done({}); 
+    }
+} else {
+    $done({});
+}
